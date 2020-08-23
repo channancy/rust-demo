be830ab15c754b630d2e14c4e14f554c3bb67114
Nancy Chan <nancychannc@gmail.com>
2020-08-22 22:35:50 -0700

    Display get request data

--- www/index.js
+++ www/index.js
@@ -1,12 +1,23 @@
 const rust = import('../pkg');
 
-rust
-  .then(m => {
-      return m.run("rustwasm/wasm-bindgen").then((data) => {
-          console.log(data);
+const content = new class {
+  constructor() {
+        this.content = document.getElementById("content");
+    }
 
-          console.log("The latest commit to the wasm-bindgen %s branch is:", data.name);
-          console.log("%s, authored by %s <%s>", data.commit.sha, data.commit.commit.author.name, data.commit.commit.author.email);
-      })
-  })
-  .catch(console.error);
+    render() {
+        rust
+        .then(m => {
+            return m.run("channancy/rust-demo").then((data) => {
+                console.log(data);
+                this.content.textContent = data.commit.commit.author.email;
+
+                console.log("The latest commit to the wasm-bindgen %s branch is:", data.name);
+                console.log("%s, authored by %s <%s>", data.commit.sha, data.commit.commit.author.name, data.commit.commit.author.email);
+            })
+        })
+        .catch(console.error);
+    }
+};
+
+content.render();
