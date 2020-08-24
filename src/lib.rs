4e0d36159eb1d9b1da9a89d92169e2296de1c87c
Nancy Chan <nancychannc@gmail.com>
2020-08-23 22:22:22 -0700

    Call color API and draw rectangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,28 @@
+use std::f64;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{Request, RequestInit, RequestMode, Response};
 
-/// A struct to hold some data from the github Branch API.
-///
-/// Note how we don't have to define every member -- serde will ignore extra
-/// data when deserializing
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Branch {
-    pub name: String,
-    pub commit: Commit,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Commit {
-    pub sha: String,
-    pub commit: CommitDetails,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CommitDetails {
-    pub author: Signature,
-    pub committer: Signature,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Signature {
-    pub name: String,
-    pub email: String,
+pub struct Color {
+    pub new_color: String,
 }
 
 #[wasm_bindgen]
-pub async fn run(repo: String) -> Result<JsValue, JsValue> {
+pub async fn run() -> Result<JsValue, JsValue> {
     let mut opts = RequestInit::new();
     opts.method("GET");
     opts.mode(RequestMode::Cors);
 
-    let url = format!("https://api.github.com/repos/{}/branches/master", repo);
+    let url = format!("http://www.colr.org/json/color/random");
 
     let request = Request::new_with_str_and_init(&url, &opts)?;
 
     request
         .headers()
-        .set("Accept", "application/vnd.github.v3+json")?;
+        .set("Accept", "application/json")?;
 
     let window = web_sys::window().unwrap();
     let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
@@ -57,8 +35,55 @@ pub async fn run(repo: String) -> Result<JsValue, JsValue> {
     let json = JsFuture::from(resp.json()?).await?;
 
     // Use serde to parse the JSON into a struct.
-    let branch_info: Branch = json.into_serde().unwrap();
+    let color_info: Color = json.into_serde().unwrap();
 
-    // Send the `Branch` struct back to JS as an `Object`.
-    Ok(JsValue::from_serde(&branch_info).unwrap())
+    // Send the struct back to JS as an `Object`.
+    Ok(JsValue::from_serde(&color_info).unwrap())
+}
+
+#[wasm_bindgen]
+pub fn draw() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.set_fill_style(&"#e3efdd".into());
+    // context.set_fill_style(&"rgb(150,50,0)".into());        
+    
+    context.fill_rect(15.0, 15.0, 80.0, 80.0);
+
+    // context.begin_path();
+
+    // // Draw the outer circle.
+    // context
+    //     .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+    //     .unwrap();
+
+    // // Draw the mouth.
+    // context.move_to(110.0, 75.0);
+    // context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    // // Draw the left eye.
+    // context.move_to(65.0, 65.0);
+    // context
+    //     .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+    //     .unwrap();
+
+    // // Draw the right eye.
+    // context.move_to(95.0, 65.0);
+    // context
+    //     .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+    //     .unwrap();
+
+    // context.stroke();
 }
--- www/index.js
+++ www/index.js
@@ -8,12 +8,11 @@ const content = new class {
     render() {
         rust
         .then(m => {
-            return m.run("channancy/rust-demo").then((data) => {
+            return m.run().then((data) => {
                 console.log(data);
-                this.content.textContent = data.commit.commit.author.email;
+                this.content.textContent = data.new_color;
 
-                console.log("The latest commit to the wasm-bindgen %s branch is:", data.name);
-                console.log("%s, authored by %s <%s>", data.commit.sha, data.commit.commit.author.name, data.commit.commit.author.email);
+                m.draw();
             })
         })
         .catch(console.error);
